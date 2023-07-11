fdc2c2e98d0bf97fc33cc49cea145e88c66ef36b
Eliecer García Rey <egarciarey1981@gmail.com>
2023-07-11 20:52:38 +0200

    first commit with the package

---
+++ math2_egarciarey1981/math2.py
@@ -0,0 +1,5 @@
+class Math2():
+    def add(self, x:int, y:int) -> int:
+        return x + y
+    def sub(self, x:int, y:int) -> int:
+        return x - y
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math2_egarciarey1981',
+    description='My second pip package',
+    version='0.1.0',
+    url='https://github.com/egarciarey1981/my-second-pip-package',
+    packages=find_packages(),
+)
